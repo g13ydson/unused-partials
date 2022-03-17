93a5040bbd603e1faf8eed7d29dfffc9026e23d9
Gleydson <tavares.gleydson@gmail.com>
2022-03-16 22:37:20 -0300

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+ruby "3.0.2"
+
+group :development do
+  gem "awesome_print"
+  gem "byebug"
+  gem "standard"
+end
---
+++ app.rb
@@ -0,0 +1,129 @@
+require "byebug"
+
+class UnusedPartial
+  EXT = %w[.html.erb .text.erb .pdf.erb .erb .html.haml .text.haml .haml .rhtml .html.slim slim]
+
+  def initialize(view_path:)
+    @view_path = view_path
+  end
+
+  def find
+    tree, dynamic = Dir.chdir(@view_path) { used_partials("views") }
+    tree.each do |idx, level|
+      indent = " " * idx * 2
+      h_indent = idx == 1 ? "" : "\n" + " " * (idx - 1) * 2
+
+      if idx == 1
+        puts "#{h_indent}The following partials are not referenced directly by any code:"
+      else
+        puts "#{h_indent}The following partials are only referenced directly by the partials above:"
+      end
+      level[:unused].sort.each do |partial|
+        puts "#{indent}#{partial}"
+      end
+    end
+
+    unless dynamic.empty?
+      puts "\n\nSome of the partials above (at any level) might be referenced dynamically by the following lines of code:"
+      dynamic.sort.map do |file, lines|
+        lines.each do |line|
+          puts "  #{file}:#{line}"
+        end
+      end
+    end
+  end
+
+  def used_partials(root)
+    raise "#{Dir.pwd} does not have '#{root}' directory" unless File.directory? root
+    files = []
+    each_file(root) do |file|
+      files << file
+    end
+    tree = {}
+    level = 1
+    existent = existent_partials(root)
+    top_dynamic = nil
+    loop do
+      used, dynamic = process_partials(files)
+      break if level > 1 && used.size == tree[level - 1][:used].size
+      tree[level] = {
+        used: used
+      }
+      if level == 1
+        top_dynamic = dynamic
+        tree[level][:unused] = existent - used
+      else
+        tree[level][:unused] = tree[level - 1][:used] - used
+      end
+      break unless (files - tree[level][:unused]).size < files.size
+      files -= tree[level][:unused]
+      level += 1
+    end
+    [tree, top_dynamic]
+  end
+
+  def existent_partials(root)
+    partials = []
+    each_file(root) do |file|
+      if /^.*\/_.*$/.match?(file)
+        partials << file.strip
+      end
+    end
+
+    partials
+  end
+
+  def each_file(root, &block)
+    files = Dir.glob("#{root}/*")
+    files.each do |file|
+      if File.directory? file
+        each_file(file) { |file| yield file }
+      else
+        yield file
+      end
+    end
+  end
+
+  def process_partials(files)
+    filename = /[a-zA-Z\d_\/]+?/
+    extension = /\.\w+/
+    partial = /:partial\s*=>\s*|partial:\s*/
+    render = /\brender\s*(?:\(\s*)?/
+    esi_include = /\besi_include\s*(?:\(\s*)?/
+    partials = []
+    dynamic = {}
+    files.each do |file|
+      File.open(file) do |f|
+        f.each do |line|
+          line = line
+            .encode("UTF-8", "binary", invalid: :replace, undef: :replace, replace: "")
+            .strip
+
+          if line =~ %r{(?:#{partial}|#{render}|#{esi_include})(['"])/?(#{filename})#{extension}*\1}
+            match = $2
+            if match.index("/")
+
+              path = match.split("/")[0...-1].join("/")
+              file_name = "_#{match.split("/")[-1]}"
+
+              full_path = "views/#{path}/#{file_name}"
+            else
+              full_path = "#{file.split("/")[0...-1].join("/")}/_#{match}"
+            end
+            partials << check_extension_path(full_path)
+          elsif /#{partial}|#{render}["']/.match?(line)
+            dynamic[file] ||= []
+            dynamic[file] << line
+          end
+        end
+      end
+    end
+    partials.uniq!
+    [partials, dynamic]
+  end
+
+  def check_extension_path(file)
+    "#{file}#{EXT.find { |e| File.exist? file + e }}"
+  end
+end
+UnusedPartial.new(view_path: ARGV.first).find
